37dee0d0c591990bd95ebb817cd75ee447aceac5
seungyong88 <gosng2001@naver.com>
2020-06-05 01:34:55 +0900

    수정 124 나라

--- programmers/모든문제/level2/124나라.js
+++ programmers/모든문제/level2/124나라.js
@@ -4,6 +4,7 @@ function solution(n) {
 
   while (n) {
     answer = array1_2_4[n % 3] + answer;
+    let temp = Math.floor((n - 1) / 3);
     n = Math.floor((n - 1) / 3);
   }
 
@@ -16,7 +17,7 @@ function solution(n) {
 // console.log(solution(6));
 
 console.log(solution(14));
-console.log(solution(15));
-console.log(solution(16));
+// console.log(solution(15));
+// console.log(solution(16));
 
 // console.log(3/3);
