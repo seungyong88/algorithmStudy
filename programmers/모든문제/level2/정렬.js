f169abc25a7acb90a32737d3f59ed45ec612f6e7
seungyong88 <gosng2001@naver.com>
2020-06-07 00:27:08 +0900

    level2 정렬

---
+++ programmers/모든문제/level2/정렬.js
@@ -0,0 +1,12 @@
+function solution(citations) {
+    var answer = 0;
+
+    citations.sort((a, b) => b - a);
+
+    for(let i = 0; i < citations.length; i++) {
+        if(citations[i] > i) answer++;
+        else break;
+    }
+
+    return answer;
+}
